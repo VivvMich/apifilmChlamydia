5a4f60a76cdde70c6db7edcdde5d708202a94b26
Sky Runner <aurelien.ernst@laposte.net>
2024-12-20 12:34:52 +0100

    premier commit

---
+++ view/movie_view.php
@@ -0,0 +1,45 @@
+
+<?php
+include "base.php";
+?>
+
+<div class="container mx-auto p-4 p-xl-5 mt-5 my-5">
+    <div class="row">
+        <h1 class="text-center col-12 mb-4">Titre du film</h1>
+
+        <div class="d-flex flex-column flex-md-row">
+            <div class="mx-3 mb-3 mb-md-0">
+                <img src="https://placehold.co/600x600" alt="Jacket du film" class="img-fluid">
+            </div>
+            <div class="col-12 col-md-6">
+                <div class="mx-3">
+                    <div class="my-5">
+                        <h2>Genres</h2>
+                        <ul class="list-group list-group-horizontal flex-wrap">
+                            <li class="list-group-item">lorem</li>
+                            <li class="list-group-item">ipsum</li>
+                        </ul>
+                    </div>
+                    <div class="my-5">
+                        <h2>Acteurs</h2>
+                        <ul class="list-group list-group-horizontal flex-wrap">
+                            <li class="list-group-item">lorem</li>
+                            <li class="list-group-item">ipsum</li>
+                        </ul>
+                    </div>
+                    <div class="my-5">
+                        <h2>Synopsis</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis modi ipsum aliquid rem ratione laborum quos deleniti expedita temporibus error laudantium labore, possimus officiis asperiores assumenda molestias perferendis rerum pariatur.
+                            Eos perspiciatis ut architecto consequuntur officia rem repellat qui iure ipsum, quisquam atque ullam nam at minus error, corrupti nihil porro maiores aspernatur ea. Culpa excepturi iusto eius sapiente ullam!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php 
+include "footer.php";
+?>
